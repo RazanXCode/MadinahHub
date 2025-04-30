64cc72eaf7e660ed1c09249655485b0471eb1118
Ahmed-Fa1sal <DerRiese765@gmail.com>
2025-04-30 11:34:45 +0300

    Creating new branch to fix bugged cached folders.

---
+++ Backend/MHBackend/Data/MyAppDbContext.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using MHBackend.Models;
+
+namespace MHBackend.Data
+{ 
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext(DbContextOptions<MyAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure the User entity
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.FirebaseUid)
+                .IsUnique();
+                
+            // Make UserIdPublic unique as well
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.PublicUserId)
+                .IsUnique();
+                
+            // Configure the enum to be stored as a string
+            modelBuilder.Entity<User>()
+                .Property(u => u.Role)
+                .HasConversion<string>();
+        }
+    }
+}
