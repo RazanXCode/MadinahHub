d99b855126a32ac2dbfac8e60055def3f6df34df
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-04-30 10:02:26 +0300

    fix conflict

--- Frontend/MHFrontend/src/app/app.config.ts
+++ Frontend/MHFrontend/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { providePrimeNG } from 'primeng/config';
@@ -8,21 +7,23 @@ import { MyPreset } from '../assets/themes/mytheme';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [    providePrimeNG({
-    theme: {
-      preset: MyPreset,
-      options: {
-        prefix: 'p',
-        darkModeSelector: '.dark',
-        cssLayer: {
-          name: 'primeng',
-          order: 'theme, base, primeng',
+  providers: [
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          prefix: 'p',
+          darkModeSelector: '.dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
         },
       },
-    },
-  }),
-  provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes), 
-  provideClientHydration(withEventReplay()), 
-  provideHttpClient(withFetch())],
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+  ],
 };
